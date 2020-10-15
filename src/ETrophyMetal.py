81979e5a951a5df441c7dd98ecbd57ae58bfaf1d
Nick Alexander <alexander50@live.com>
2020-10-14 23:03:41 -0400

    Refactored

--- src/ETrophyMetal.py
+++ src/ETrophyMetal.py
@@ -3,11 +3,51 @@ from enum import Enum, unique
 
 @unique
 class ETrophyMetal(Enum):
-    Bronze = 'Bronze'
-    Silver = 'Silver'
-    Gold = 'Gold'
-    Platinum = 'Platinum'
 
+    Bronze = 0
+    Silver = 1
+    Gold = 2
+    Platinum = 3
 
-def allMetals():
-    return [metal.name for metal in ETrophyMetal]
+    @staticmethod
+    def from_str(str_value: str):
+        options = {
+            'Bronze': ETrophyMetal.Bronze,
+            'Silver': ETrophyMetal.Silver,
+            'Gold': ETrophyMetal.Gold,
+            'Platinum': ETrophyMetal.Platinum,
+        }
+
+        str_value = str_value.strip().title()
+
+        if str_value in options:
+            return options[str_value]
+        else:
+            return None
+
+    def __str__(self):
+        if self is ETrophyMetal.Bronze:
+            return 'Bronze'
+        elif self is ETrophyMetal.Silver:
+            return 'Silver'
+        elif self is ETrophyMetal.Gold:
+            return 'Gold'
+        elif self is ETrophyMetal.Platinum:
+            return 'Platinum'
+        return None
+
+    def __lt__(self, other):
+        if not isinstance(self, ETrophyMetal) or not isinstance(other, ETrophyMetal):
+            return False
+        return self.value < other.value
+
+    def __eq__(self, other):
+        if not isinstance(self, ETrophyMetal) or not isinstance(other, ETrophyMetal):
+            return False
+        return self.value < other.value
+
+    def __gt__(self, other):
+        if not isinstance(self, ETrophyMetal) or not isinstance(other, ETrophyMetal):
+            return False
+        return self.value < other.value
+        
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import sys
+import re
 from os import getcwd
 from os.path import join
 
 from ETrophyMetadataCategory import ETrophyMetadataCategory
-from ETrophyMetal import ETrophyMetal, allMetals
+from ETrophyMetal import ETrophyMetal
 
 
 def parse_args():
@@ -19,96 +20,62 @@ def parse_args():
         return (input_path, output_path)
 
 
-def main(input_file, output_file):
-    output_file.write(header())
+def line_is_date(line):
+    date_pattern = r'\d{1,2}(th|st|nd)\s*.{3}\s*20\d{2}'    
+    return re.search(date_pattern, line) is not None
 
-    is_name_next = True
-    is_criteria_next = False
-    is_unlock_date_next = False
-    is_metal_next = False
+
+def line_is_time(line):
+    time_pattern = r'\d{1,2}:\d{2}:\d{2}\s*(am|AM|pm|PM)'
+    return re.search(time_pattern, line) is not None
+
+
+def write_header_line(output_file):
+    output_file.write('unlocked,missable,name,criteria,metal')
+
+
+def write_data_line(data, output_file):
+    unlocked = ETrophyMetadataCategory.UNLOCK_DATE in data  and ETrophyMetadataCategory.UNLOCK_TIME in data
+    name = data[ETrophyMetadataCategory.NAME]
+    criteria = data[ETrophyMetadataCategory.CRITERIA]
+    metal = str(data[ETrophyMetadataCategory.METAL])
+
+    output_line = f'{unlocked},{False},{name},{criteria},{metal}\n'
+    output_file.write(output_line)
+
+
+def main(input_file, output_file):
+    # write_header_line(output_file)
+
+    all_data = []
+
+    current_data = dict()
+    current_data_index = 0
 
     for line in input_file:
-        category = None
+        category = ETrophyMetadataCategory(current_data_index)
 
-        if is_name_next:
-            category = ETrophyMetadataCategory.NAME
-        elif is_criteria_next:
-            category = ETrophyMetadataCategory.CRITERIA
-        elif is_unlock_date_next and is_unlock_date_line(line):
-            category = ETrophyMetadataCategory.UNLOCK_DATE
-        elif is_unlock_date_next and is_metal_line(line):
-            output_file.write(process_line(
-                None, ETrophyMetadataCategory.UNLOCK_DATE))
-            category = ETrophyMetadataCategory.METAL
-        elif is_metal_next and is_metal_line(line):
-            is_unlock_date_next = False
-            category = ETrophyMetadataCategory.METAL
+        if category == ETrophyMetadataCategory.UNLOCK_DATE and not line_is_date(line):
+            current_data_index += 1
+            category = ETrophyMetadataCategory(current_data_index)
+        
+        if category == ETrophyMetadataCategory.UNLOCK_TIME and not line_is_time(line):
+            current_data_index += 1
+            category = ETrophyMetadataCategory(current_data_index)
 
-        if category is not None:
-            output_file.write(process_line(line, category))
-            is_name_next = category == ETrophyMetadataCategory.METAL
-            is_criteria_next = category == ETrophyMetadataCategory.NAME
-            is_unlock_date_next = category == ETrophyMetadataCategory.CRITERIA
-            is_metal_next = category == ETrophyMetadataCategory.CRITERIA or category == ETrophyMetadataCategory.UNLOCK_DATE
+        if category == ETrophyMetadataCategory.METAL:
+            current_data[category] = ETrophyMetal.from_str(line.strip().replace(',', ''))
+            all_data.append(current_data)
+            current_data = dict()
+            current_data_index = 0
+        else:
+            current_data[category] = line.strip().replace(',', '')
+            current_data_index += 1
+    
+    ordered_data = sorted(all_data, reverse=True, key=lambda data: data[ETrophyMetadataCategory.METAL])
+    for data in ordered_data:
+        write_data_line(data, output_file)
 
-
-def header():
-    return 'name,criteria,unlocked,metal\n'
-
-
-def process_line(line, category):
-    def quote(s): return '"' + s + '"'
-
-    if category == ETrophyMetadataCategory.UNLOCK_DATE:
-        return quote(str(line is not None)) + ','
-    elif category == ETrophyMetadataCategory.NAME:
-        return quote(line.strip()) + ','
-    elif category == ETrophyMetadataCategory.CRITERIA:
-        return quote(line.strip()) + ','
-    elif category == ETrophyMetadataCategory.METAL:
-        return quote(line.strip().lower().capitalize()) + '\n'
-    else:
-        return ''
-
-
-def is_unlock_date_line(line):
-    MONTHS = [
-        'jan',
-        'feb',
-        'mar',
-        'apr',
-        'may',
-        'jun',
-        'jul',
-        'aug',
-        'sep',
-        'oct',
-        'nov',
-        'dec'
-    ]
-
-    normalized = line.strip().lower().capitalize().replace('st', '').replace('nd', '').replace('rd', '').replace('th', '')
-    components = normalized.split(' ')
-
-    if len(components) != 3:
-        return False
-
-    day, month, year = components
-
-    if int(day) < 1 or int(day) > 31:
-        return False
-    if month not in MONTHS:
-        return False
-    if int(year) < 2000:
-        return False
-
-    return True
-
-
-def is_metal_line(line):
-    normalized = line.strip().lower().capitalize()
-    metals = allMetals()
-    return normalized in metals
 
 
 if __name__ == '__main__':
